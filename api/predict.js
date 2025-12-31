3a3b511c8e1c77c415b14a3ee45328a39834a1f8
sutharankit679 <sutharankit679@gmail.com>
2025-12-31 20:13:53 +0530

    Create predict.js

---
+++ api/predict.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const BACKEND_URL =
+      "https://ml-model-backend-rczq.onrender.com/api/predict";
+
+    const response = await fetch(BACKEND_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Backend error" });
+  }
+}
